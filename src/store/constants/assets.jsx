74f11f0c7ec572f8e82c6dcc9165af09fc9a22d7
VŨ NGỌC HẢI HÀ <vungochaiha@gmail.com>
2024-01-02 17:58:29 +0700

    assets management

---
+++ src/store/constants/assets.jsx
@@ -0,0 +1,10 @@
+const assetsPath = "./src/assets/";
+const iconPath = assetsPath + "icons/";
+
+const Assets = {
+  icon: {
+    react: iconPath + "react.svg",
+    vite: iconPath + "vite.svg",
+  },
+};
+export default Assets;
